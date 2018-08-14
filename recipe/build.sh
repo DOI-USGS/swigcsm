159bea8708e6c4064bf119497a89e90db6d6e007
jay <jlaura@usgs.gov>
2018-08-14 13:54:44 -0700

    Adds a conda build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,5 @@ source activate base
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PREFIX ..
 make
-python python/setup.py install
+cd python
+python setup.py install
